bb4cbdd71ee0b2e4f687857c7b68ed7b5de3ba33
errorhirobrine <dvir.benshlomo@gmail.com>
2026-02-23 18:14:58 +0200

    code 1

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
+#!/user/bin/python3
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('google.com', 80))
-s.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
-response = s.recv(4096)
+import time
+import os
+import sys
---
+++ servercmd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+$ServerPath/ServerStart.sh
+echo "Server is running..."
+while true; do
+    sleep 60
+done
